ebd79c8152c4940b765635b9cd4139867059484a
HaYun <stephenkim@Stephens-MacBook-Air.local>
2023-04-17 14:14:46 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,9 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
+        <Link href="/index.html">
+          <span>Click here to see BOUNCING BALLS!</span>
+        </Link>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -113,4 +116,5 @@ export default function Home() {
       `}</style>
     </div>
   )
+
 }
